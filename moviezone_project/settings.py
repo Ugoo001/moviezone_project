594e113ad22f88a007b01ebdd57333a6b96af887
Kenenna Onwuagba <kenenna.onwuagba@ubagroup.com>
2023-04-04 16:26:47 +0100

    allowed_host

--- moviezone_project/settings.py
+++ moviezone_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t1*(dr2nnxn4h^7=9ytdi*6hdgp(23r!#lx&8zx7oqx_(_mkt%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["moviezoneproject-production.up.railway.app"]
 
 
 # Application definition
