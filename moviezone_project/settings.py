92113656dda24bfdb80e1fc0be7b2427928bbb47
Kenenna Onwuagba <kenenna.onwuagba@ubagroup.com>
2023-04-06 14:36:09 +0100

    update

--- moviezone_project/settings.py
+++ moviezone_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t1*(dr2nnxn4h^7=9ytdi*6hdgp(23r!#lx&8zx7oqx_(_mkt%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["moviezoneproject-production.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'moviezone',
     'rest_framework',
+    'corsheaders',
     
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +74,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'moviezone_project.wsgi.application'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
+
 
 
 # Database
