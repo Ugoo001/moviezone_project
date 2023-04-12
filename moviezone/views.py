583c8f7f2829c85058aaef82362021687ce93c07
Ezeji Precious Ugochi <preciousugochi8@gmail.com>
2023-04-12 11:25:44 +0100

    added imdb trailer to the Response

--- moviezone/views.py
+++ moviezone/views.py
@@ -10,6 +10,7 @@ import os
 def recommend_movies(request):
     # Get genre,title and ratings
     try:
+   
         title = request.GET.get('title')
         genres = request.GET.get('genres')
         user_rating = request.GET.get('user_rating')
@@ -25,10 +26,13 @@ def recommend_movies(request):
     
         if not genres and not title and not user_rating:
             return Response({'message': 'Add a valid parameter, genres,title or user_rating'})
+        if genres and title and user_rating == None:
+            return Response({'message': 'Invalid input. Please provide a valid user rating.'})
         imdb_response = requests.get(imdb_url)
         imdb_data = imdb_response.json()
     
-
+    
+    
     #sorting the movies in descending order
         if imdb_data['results'] is not None:
             sort_movies = sorted(imdb_data['results'], key=lambda k: float(k['imDbRating']) if k.get('imDbRating') is not None else -1, reverse=True)
@@ -39,15 +43,37 @@ def recommend_movies(request):
                 recommend_movie_plot = sort_movies[0]['plot']
                 recommended_movie = sort_movies[0]['title']
                 recommended_movie_url = recommended_movie.replace(" ", "+")
+
+                recommend_movie_id = sort_movies[0]['id']
+                recommend_movie_trailer_url = f'https://imdb-api.com/en/API/Trailer/{imdb_api_key}/{recommend_movie_id}'
+                trailer_response = requests.get(recommend_movie_trailer_url)
+                trailer_data = trailer_response.json()
+                
+                if trailer_data is not None:
+                    recommend_movie_trailer = trailer_data['link']
+                    trailer_thumbnail = trailer_data['thumbnailUrl']
+                    trailer_title = trailer_data['videoTitle']
         
                 netflix_url = f'https://www.netflix.com/search?q={recommended_movie_url}'
                 netflix_response = requests.get(netflix_url)
                 if netflix_response.status_code == 200:
-                    return Response({'name': recommended_movie, 'url': netflix_url, 'image':recommend_movie_image, 'plot': recommend_movie_plot})
+                    return Response({'name': recommended_movie,
+                                      'url': netflix_url, 
+                                      'image':recommend_movie_image, 
+                                      'plot': recommend_movie_plot,
+                                      'trailer': recommend_movie_trailer,
+                                      'thumbnail': trailer_thumbnail,
+                                      'trailer_title': trailer_title,
+                                      })
                 else:
-                    return Response({'message': f'Your recommended movie is {recommended_movie}. Movie is not available on netflix', 'image': recommend_movie_image, 'plot': recommend_movie_plot})
+                    return Response({'message': f'Your recommended movie is {recommended_movie}. Movie is not available on netflix', 
+                                     'image': recommend_movie_image, 
+                                     'plot': recommend_movie_plot,
+                                     'trailer': recommend_movie_trailer,
+                                     'thumbnail': trailer_thumbnail,
+                                    })
+                
         else:
             return Response({'message': 'No movies found with the given criteria'})    
-
     except Exception as e:
         return Response({'message': f'Error: {str(e)}'})
